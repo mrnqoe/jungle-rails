9a3ae9784fb327aa1b5f5c786d2f06b7a0686eb9
Nicolas Moreau <mrnqoe@gmail.com>
2017-02-16 22:22:29 +0000

    Somethinggs wrong with everythin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -24,7 +24,6 @@ class Admin::UsersController < ApplicationController
     params.require(:user).permit(
     :email,
     :password,
-    :password_confirmation,
     :first_name,
     :last_name)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(
     :email,
-    :password)
-    # :first_name,
-    # :last_name)
+    :password,
+    :first_name,
+    :last_name)
   end
 
 end
