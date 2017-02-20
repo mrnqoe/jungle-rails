e88897f97c65d4eeeab587a660e1f570a5b154f3
Nicolas Moreau <mrnqoe@gmail.com>
2017-02-20 12:04:06 -0500

    rspec is bundle installed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-  def index
+  def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create, :destroy]
   end
-  resources :categories, only: [:index]
+  resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     put    :add_item
