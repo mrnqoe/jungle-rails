bfae8558db597a40606f45190e3c6318a820386c
Nicolas Moreau <mrnqoe@gmail.com>
2017-02-21 13:02:47 -0500

    product details capybara yes

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    # 3.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset("apparel1.jpg"),
+      quantity: 10,
+      price: 64.99
+    )
+    # end
+  end
+
+  scenario "They see details of one product" do
+    # ACT
+    visit root_path
+    find_link("Details").trigger('click')
+
+    # DEBUG
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show'
+    save_screenshot
+    puts page.html
+  end
+
+end
